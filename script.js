d73073c42d08960bfaf142c332d6ef193c9055e4
Joseph Kockaya <42441327+JKockaya@users.noreply.github.com>
2023-05-30 13:26:08 +0100

    Add user generated canvas functionality
    Added button with click event listener that allows user to
    enter number of squares across grid will be.
    Revised previous code so that the grid generation and the
    draw ability are unique functions that can be called
    when the user creates a new canvas.

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 const container = document.getElementById('container');
-const divs = [];
 
-for (let i = 1; i <= 256; i++) {
-    divs.push(`square-${i}`);
+createGrid(16);
+gridDraw();
+
+function createGrid(num) {
+    const divs = [];
+
+    for (let i = 1; i <= num * num; i++) {
+        divs.push(`square-${i}`);
+    }
+
+    divs.forEach(function(e) {
+        container.innerHTML += 
+        `<div class="square" id="${e}"></div>`;
+    })
+
+    const squareSize = document.querySelectorAll('.square');
+    const size = 640 / num;
+
+    for (let square of squareSize) {
+    square.style.width = `${size}px`;
+    square.style.height = `${size}px`;
+    }
 }
 
-divs.forEach(function(e) {
-    container.innerHTML += 
-    `<div class="square" id="${e}"></div>`;
-})
-
-const squares = document.querySelectorAll('.square');
+function gridDraw() {
+    const squares = document.querySelectorAll('.square');
 
 for (const square of squares) {
     square.addEventListener('mouseover', () => {
         square.style.backgroundColor = 'pink';
     })
 }
+
+}
+
+
+const genBtn = document.querySelector('button');
+
+genBtn.addEventListener('click', newCanvas);
+
+function newCanvas() {
+    const cellNum = prompt("Enter a grid size up to 100: ");
+    container.innerHTML = ``;
+    createGrid(cellNum);
+    gridDraw();
+
+
+    
+    
+
+} 
